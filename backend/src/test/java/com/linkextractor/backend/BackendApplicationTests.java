cebf82b68e83406c1f81ee8aaf2b69e638fbd084
Mortada M'Rabet <mortada.mrabet@hva.nl>
2023-11-21 18:09:00 +0100

    refactor(backend), initial backend

---
+++ backend/src/test/java/com/linkextractor/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkextractor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
