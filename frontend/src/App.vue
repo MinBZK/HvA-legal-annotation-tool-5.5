9e5b637deff318bb74b7d9cb5b7e9941c133cbe5
Yazan Mousa <yazanmousa@re-byods-145-28-186-92.wireless.hva.nl>
2023-11-22 15:55:47 +0100

    xmlloader

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,20 @@
-<template>
-  <router-view />
-</template>
+<!--<template>-->
+<!--  <router-view />-->
+<!--</template>-->
 
-<script setup>
-  //
+<!--<script setup>-->
+<!--  //-->
+<!--</script>-->
+
+<template>
+  <div>
+    <xml-loader />
+  </div>
+</template>
+<script>
+
+import XmlLoader from "./components/XmlLoader.vue";
+export default {
+  components: {XmlLoader}
+}
 </script>
