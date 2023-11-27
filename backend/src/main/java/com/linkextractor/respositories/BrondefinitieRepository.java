7809e807fe9be4f848ab6abbbc355c77467c6174
Mortada M'Rabet <mortada.mrabet@hva.nl>
2023-11-27 10:30:10 +0100

    refactor(./backend), added controllers for Rechtsfeit and rechtsbetrekking

---
+++ backend/src/main/java/com/linkextractor/respositories/BrondefinitieRepository.java
@@ -0,0 +1,18 @@
+package com.linkextractor.respositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.linkextractor.models.Brondefinitie;
+
+public interface BrondefinitieRepository extends CrudRepository<Brondefinitie, Integer>{
+    
+    List<Brondefinitie> findAll();
+
+    Brondefinitie findOne(int brondefinitie_id);
+
+    Brondefinitie save(Brondefinitie brondefinitie);
+
+    void deleteById(int brondefinitie_id);
+}
