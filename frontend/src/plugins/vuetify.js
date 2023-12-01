65d8fd1e4a643e1c10cf5dbed8834bf07578e760
Yazan Mousa <yazanmousa@yazans-mbp.kpn>
2023-12-01 20:19:50 +0100

    Back to normal

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,48 +1,17 @@
-// /**
-//  * plugins/vuetify.js
-//  *
-//  * Framework documentation: https://vuetifyjs.com`
-//  */
-//
-// // Styles
-// import '@mdi/font/css/materialdesignicons.css'
-// import 'vuetify/styles'
-// import Vue from 'vue';
-// import Vuetify from 'vuetify/lib/framework';
-//
-// Vue.use(Vuetify);
-//
-// // Composables
-// import { createVuetify } from 'vuetify'
-//
-// // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-// export default createVuetify({
-//   theme: {
-//     themes: {
-//       light: {
-//         colors: {
-//           primary: '#1867C0',
-//           secondary: '#5CBBF6',
-//         },
-//       },
-//     },
-//   },
-// })
-// plugins/vuetify.js
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
 
-// plugins/vuetify.js
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 
-// Other imports...
+// Composables
+import { createVuetify } from 'vuetify'
 
-// Change the import statement to use a named import
-import { createApp } from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-
-const app = createApp();
-app.use(Vuetify);
-
-// Other configurations...
-
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     themes: {
@@ -54,4 +23,4 @@ export default createVuetify({
       },
     },
   },
-});
+})
