ea104c5f46f243281dc2653b5ab2e03e7a1d0f77
Emre <emre.simsek@hva.nl>
2023-12-06 12:48:52 +0100

    feat: Implement basic login/register endpoints with role-based JWT token authentication

--- backend/src/main/java/com/linkextractor/BackendApplication.java
+++ backend/src/main/java/com/linkextractor/BackendApplication.java
@@ -1,7 +1,17 @@
 package com.linkextractor;
 
+import com.linkextractor.models.Role;
+import com.linkextractor.models.User;
+import com.linkextractor.respositories.RoleRepository;
+import com.linkextractor.respositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +20,21 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	// Bean definition for initializing default roles and an admin user (For testing purposes)
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncode){
+		return args ->{
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			// Creating an admin user with a default password (for testing purposes)
+			User admin = new User(1, "admin", passwordEncode.encode("password"), roles);
+
+			userRepository.save(admin);
+		};
+	}
 }
---
+++ backend/src/main/java/com/linkextractor/models/Role.java
@@ -0,0 +1,52 @@
+package com.linkextractor.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="role_id")
+    private int roleId;
+
+    private String authority;
+
+    public Role(){
+        super();
+    }
+
+    public Role(String authority){
+        this.authority = authority;
+    }
+
+    public Role(int roleId, String authority){
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority){
+        this.authority = authority;
+    }
+
+    public int getRoleId(){
+        return this.roleId;
+    }
+
+    public void setRoleId(Integer roleId){
+        this.roleId = roleId;
+    }
+}
