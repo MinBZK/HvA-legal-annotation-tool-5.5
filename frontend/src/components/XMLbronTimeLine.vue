e89b0ecdac827471cda6e3ce300a70df125eb7b7
Mortada M'Rabet <mortada.mrabet@hva.nl>
2024-01-01 21:10:19 +0100

    refactor(XMLbrontimeline.vue) added modals for editing, warning

--- frontend/src/components/XMLbronTimeLine.vue
+++ frontend/src/components/XMLbronTimeLine.vue
@@ -11,31 +11,41 @@
                         <p> {{ item.date }}</p>
                     </v-card-text>
                     <v-card-actions>
-                        <v-btn>
+                        <v-btn @click="showDeleteAlert(item)">
                             Delete
                         </v-btn>
-                        <v-btn @click="openModal">
+                        <v-dialog v-model="deleteDialog" max-width="400">
+                            <v-card>
+                                <v-card-title class="headline">Warning</v-card-title>
+                                <v-card-text>
+                                    Are you sure you want to delete: {{ selectedModalItem.name }}
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-btn @click="handleAction('delete', selectedModalItem.id)">OK</v-btn>
+                                    <v-btn @click="handleAction('Cancel', selectedModalItem.id)">Cancel</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-dialog>
+                        <v-btn @click="openModal(item)">
                             Preview
                         </v-btn>
                         <v-dialog v-model="modalOpen" max-width="600">
                             <v-card>
                                 <v-card-title>
-                                    {{ item.name }}
+                                    {{ selectedModalItem.name }}
                                 </v-card-title>
                                 <v-card-text>
-                                    <!-- Modal content goes here -->
-                                    This is the content of the modal.
+                                    Full text description
                                 </v-card-text>
                                 <v-card-actions>
                                     <v-btn @click="closeModal">Close</v-btn>
+                                    <v-btn>Edit</v-btn>
                                 </v-card-actions>
                             </v-card>
                         </v-dialog>
-                        <v-btn>
-                            Set
-                        </v-btn>
                     </v-card-actions>
-                </v-card> </v-timeline-item>
+                </v-card>
+            </v-timeline-item>
         </v-timeline>
     </v-container>>
 </template>
@@ -43,21 +53,50 @@
 <script>
 
 import { xmlBronStore } from "@/store/xmlbron"
+import axios from "axios";
 export default {
     props: {
-        timelineData: Array, // Assuming you pass timelineData as a prop
+        timelineData: Array,
     },
     data() {
         return {
             modalOpen: false,
+            deleteDialog: false,
+            selectedModalItem: {},
         };
     },
     methods: {
-        openModal() {
+        openModal(item) {
             this.modalOpen = true;
+            this.selectedModalItem = item;
+            console.log(item)
+        },
+        //role check needs to be added
+        showDeleteAlert(item){
+            this.deleteDialog = true;
+            this.selectedModalItem = item;
         },
         closeModal() {
             this.modalOpen = false;
+        },
+        //role check needs to be added
+        async handleAction(action, id){
+            if(action == "delete"){
+                try {
+                    await axios.delete(`http://localhost:8085/XMLbron/${id}`)
+                } catch(error){
+                    console.error()
+                }
+
+                this.deleteDialog = false;
+            }
+            this.deleteDialog = false;
+        },
+        deleteArticle() {
+            return null;
+        },
+        editArticle() {
+            return null;
         }
     }
 };
