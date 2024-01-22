7a69b4cb119ee0c51eb11e6b4435cd2943d4d20d
Emre <emre.simsek@hva.nl>
2024-01-22 16:02:23 +0100

    test: little change

--- frontend/tests/spec/AppLogin.spec.js
+++ frontend/tests/spec/AppLogin.spec.js
@@ -1,11 +1,31 @@
-import { test, expect } from 'vitest';
+import { expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import LoginComponent from '@/components/AppLogin.vue';
 import { createVuetify } from 'vuetify';
-import { createPinia } from 'pinia';
+
+import { createPinia, setActivePinia } from 'pinia';
+
+
+// Stub XMLSerializer to prevent interference with serialization
+class StubbedXMLSerializer {
+  serializeToString() {
+    return '';
+  }
+}
+
+global.XMLSerializer = StubbedXMLSerializer;
+
+// Before each test
+beforeEach(() => {
+  const pinia = createPinia();
+  setActivePinia(pinia);
+});
+
+const vuetify = createVuetify();
+
+
 
 test('renders login component correctly', () => {
-  const vuetify = createVuetify();
 
   const wrapper = mount(LoginComponent, {
     global: {
@@ -17,7 +37,6 @@ test('renders login component correctly', () => {
 });
 
 test('initializes with correct data', () => {
-  const vuetify = createVuetify();
 
   const wrapper = mount(LoginComponent, {
     global: {
@@ -37,7 +56,6 @@ test('initializes with correct data', () => {
 });
 
 test('validates form inputs correctly', async () => {
-  const vuetify = createVuetify();
 
   const wrapper = mount(LoginComponent, {
     global: {
@@ -58,7 +76,7 @@ test('validates form inputs correctly', async () => {
 });
 
 test('updates data on user input', async () => {
-  const vuetify = createVuetify();
+
   const wrapper = mount(LoginComponent, {
     global: {
       plugins: [vuetify, createPinia()],
@@ -74,6 +92,35 @@ test('updates data on user input', async () => {
   // Assert that the component's data is updated
   expect(wrapper.vm.username).toBe('testuser');
   expect(wrapper.vm.password).toBe('testpassword');
+});
+
+test('submits login form when all fields are filled correctly', async () => {
+  const vuetify = createVuetify();
+  const wrapper = mount(LoginComponent, {
+    global: {
+      plugins: [vuetify, createPinia()],
+    },
+  });
+
+  // Set valid input values for the login form
+  await wrapper.setData({
+    username: 'emre',
+    password: 'emre',
+  });
+
+  // Trigger form submission
+  await wrapper.find('form').trigger('submit.prevent');
+
+  // Wait for asynchronous tasks to complete
+  await wrapper.vm.$nextTick();
+
+  // Wait for 3 seconds (adjust as needed)
+  await new Promise(resolve => setTimeout(resolve, 3000));
+
+  // Assert the expected outcomes after successful login
+  expect(wrapper.html().replace(/\s/g, '')).toContain('Inloggen succesvol');
+  expect(wrapper.vm.loginSuccess).toBe(true);
+  expect(wrapper.vm.loginSuccessMessage).toBe('Inloggen succesvol')
 });
 
 
